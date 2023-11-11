eae8817ec5cf6560edc887e7ec9fc829f4fb8247
Manuele Pesenti <manuele.pesenti@gmail.com>
2023-11-11 16:41:41 +0100

    fix

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,6 +3,7 @@ from py4web import action, request
 from .common import logger
 
 @action("index", method=['GET','POST'])
-def index():
-    logger.info(json.dumps(request.json, indent=4))
+@action("log/<action>", method=['GET','POST'])
+def index(action:str='index'):
+    logger.info(f'Hook received to action {action} with payload: {json.dumps(request.json, indent=4)}')
     return "Hello World"
